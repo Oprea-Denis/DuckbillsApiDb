2f7cc03a182c536df0be7694666487eb9e3e09ca
Oprea Denis <denis.oprea@emanuel.ro>
2021-05-05 20:06:40 +0300

    Add project files.

---
+++ Data/WebApplication5Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication5.Models;
+
+namespace WebApplication5.Data
+{
+    public class WebApplication5Context : DbContext
+    {
+        public DbSet<WebApplication5.Models.Duckbill> Duckbill { get; set; }
+        public WebApplication5Context (DbContextOptions<WebApplication5Context> options)
+            : base(options)
+        { }
+    }
+}
